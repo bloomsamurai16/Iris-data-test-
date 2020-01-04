600c0247ba520e091e47e4da328fc98d7065cdbe
bloomsamurai16 <42292347+bloomsamurai16@users.noreply.github.com>
2020-01-04 14:25:15 +0400

    Update check_accuracy.py

--- check_accuracy.py
+++ check_accuracy.py
@@ -1,6 +1,6 @@
 #!usr/bin/python3
 
-from  sklearn.datasets import  load_iris 
+from sklearn.datasets import load_iris 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
